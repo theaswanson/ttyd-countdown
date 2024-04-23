d8598ffbefae1a71f93486ac96dfa3cc04ad3da1
Adam Swanson <adam.swanson.98@gmail.com>
2024-04-22 21:04:50 -0400

    Add TTYD countdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Countdown from "react-countdown";
+import "./App.css";
+import marioImg from "./assets/mario.webp";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={marioImg} className='logo mario' alt='Paper Mario' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className='card'>
+        <Countdown
+          date={new Date(2024, 4, 23)}
+          renderer={({ days, hours, minutes, seconds, completed }) => {
+            if (completed) {
+              return <h1>The game is live! Go play!</h1>;
+            }
+
+            const displayDays = days > 0;
+            const displayHours = hours > 0 || days > 0;
+            const displayMinutes = minutes > 0 || hours > 0 || days > 0;
+            const displaySeconds =
+              seconds > 0 || minutes > 0 || hours > 0 || days > 0;
+
+            const countdown: string[] = [];
+
+            if (displayDays) {
+              countdown.push(`${days} ${days === 1 ? "day" : "days"}`);
+            }
+
+            if (displayHours) {
+              countdown.push(`${hours} ${hours === 1 ? "hours" : "hours"}`);
+            }
+
+            if (displayMinutes) {
+              countdown.push(
+                `${minutes} ${minutes === 1 ? "minute" : "minutes"}`
+              );
+            }
+
+            if (displaySeconds) {
+              countdown.push(
+                `${seconds} ${seconds === 1 ? "second" : "seconds"}`
+              );
+            }
+
+            return (
+              <>
+                <h1>Paper Mario: TTYD releases in</h1>
+                <h1>{countdown.join(", ")}</h1>
+              </>
+            );
+          }}
+        ></Countdown>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
